15ca262e51cc98877897d3a9ba3b07247099754a
Rafael Lima <rafael.lima@transferwise.com>
2020-05-23 19:08:20 +0100

    Add produtos com desconto

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,2 +1,9 @@
 class ProdutosController < ApplicationController
+
+  def index
+    @produtos = Produto.order(nome: :desc)
+    @produtos_com_desconto = Produto.order(:preco).limit 1
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: "produtos#index"
+
   get "produtos", to: "produtos#index"
 end
