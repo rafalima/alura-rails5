67ae573b0fd9a53acfae63121708209e7dfa9c2a
Rafael Lima <rafael.lima@transferwise.com>
2020-05-24 12:38:52 +0100

    Refactor to add resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root to: "produtos#index"
 
-  get "produtos", to: "produtos#index"
-  get "produtos/new", to: "produtos#new"
-  post "produtos", to: "produtos#create"
-  delete "produtos/:id/remover", to: "produtos#destroy", as: :produto
+  resources :produtos, only: [:new, :create, :destroy]
 end
