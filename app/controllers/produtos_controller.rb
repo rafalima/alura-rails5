1a8594f66928e5cc92c8e465d27c6e03f7134924
Rafael Lima <rafael.lima@transferwise.com>
2020-05-24 11:52:38 +0100

    Add new html and save new product

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -5,4 +5,11 @@ class ProdutosController < ApplicationController
     @produtos_com_desconto = Produto.order(:preco).limit 1
   end
 
+  def create
+    produto = params.require(:produto).permit(:nome, :descricao, :quantidade, :preco)
+    Produto.create(produto)
+
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: "produtos#index"
 
   get "produtos", to: "produtos#index"
+  get "produtos/new", to: "produtos#new"
+  post "produtos", to: "produtos#create"
 end
